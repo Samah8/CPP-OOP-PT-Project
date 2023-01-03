08b1ce6855a28d2dcfd8973095084cd5204990e0
sherifElhabibi <109415863+sherifElhabibi@users.noreply.github.com>
2023-01-03 17:38:57 +0200

    Add files via upload

--- DEFS.h
+++ DEFS.h
@@ -8,9 +8,11 @@ enum ActionType //The actions supported (you can add more if needed)
 {
 	DRAW_SQUARE,	//Draw a Square
 	DRAW_ELPS,		//Draw an Ellipse
-	DRAW_HEX,
-	DRAW_CIR,//Draw a Hexagon
-	DRAW_TRG,
+	DRAW_RECT,		//Draw a Rectangle
+	DRAW_TRI,	    //Draw a Triangle
+	DRAW_HEX,	    //Draw a Hexagon
+	DRAW_CIRC,	    //Draw a Circle
+	SELECT,			//Select shape
 	CHNG_DRAW_CLR,	//Change the drawing color
 	CHNG_FILL_CLR,	//Change the filling color
 	CHNG_BK_CLR,	//Change background color
